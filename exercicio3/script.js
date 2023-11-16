81889e5316db2b9d3ecd9981831bc998ec53a416
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2023-11-15 22:51:15 -0300

    Resolvendo os exercícios de string

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,17 @@
 //Crie a const para a frase aqui
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"";
+
+const mudaFrase = (frase) => {
+
+  let fraseNova = frase.replaceAll("verde", "rosa", "azul");
+  fraseNova = frase.replaceAll("azul", "laranja");
+
+  let verifica = fraseNova.includes("rosa" && "laranja");
+
+  fraseNova = fraseNova.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR");
+
+  return console.log(`${fraseNova}\n ${verifica}`)
+}
+
+mudaFrase(frase);
