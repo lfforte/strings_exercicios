c31aa95fbe59210fb53ba6504f71710e9e1c9ff1
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2023-11-17 19:41:52 -0300

    corrigindo exercício 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,17 +1,18 @@
 //Crie a const para a frase aqui
 
 const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"";
-
+console.log(frase);
 const mudaFrase = (frase) => {
 
-  let fraseNova = frase.replaceAll("verde", "rosa", "azul");
-  fraseNova = frase.replaceAll("azul", "laranja");
+  let fraseNova = frase.replace("verde", "rosa");
+  let fraseNova1 = fraseNova.replace("azul", "laranja");
 
-  let verifica = fraseNova.includes("rosa" && "laranja");
+  let verifica = fraseNova1.includes("rosa");
+  let verifica1 = fraseNova1.includes("laranja");
 
-  fraseNova = fraseNova.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR");
+  fraseNova1 = fraseNova1.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR");
 
-  return console.log(`${fraseNova}\n ${verifica}`)
+  return console.log(`${fraseNova1}\n ${verifica && verifica1}`)
 }
 
 mudaFrase(frase);
